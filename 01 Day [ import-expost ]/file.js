61aaca93f6a8973d6ca986d489d52d8843527bea
PIYUSHDAHLE <piyushdahle@gmail.com>
2025-01-14 23:12:55 +0530

    Require and Export

---
+++ 01 Day [ import-expost ]/file.js
@@ -0,0 +1,10 @@
+
+// module.exports ={
+//      age : 25,
+//     isMarried :false,
+//      name : 'Piyush Dahle'
+// } 
+
+const wow = 10;
+module.exports = wow;
+
---
+++ 01 Day [ import-expost ]/index.js
@@ -0,0 +1,9 @@
+const wow = require('./file.js')
+
+// console.log(a);
+// console.log(a.age);
+// console.log(a.name);
+// console.log(a.isMarried);
+
+console.log(wow);
+
